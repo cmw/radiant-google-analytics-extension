f1b215ba3352789d579f7cfc430f2d077c318439
Amr N Tamimi <amrnt0@gmail.com>
2010-04-11 22:20:35 +0300

    adding the ability to track domain with multi subdomains

--- lib/google_analytics.rb
+++ lib/google_analytics.rb
@@ -4,6 +4,9 @@ module GoogleAnalytics
 
   desc "Creates Google Analytics with id content"
   tag "google_analytics" do |tag|
+    type = tag.attr['type'] || 's' # single domain 's' OR with many subdomain 'm'
+    domian = tag.attr['domain'] # required if type == 'm' // without www. just the name, example: thisisdomain.com
+    if type == 's'
 %{<script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
@@ -13,5 +16,17 @@ try {
 var pageTracker = _gat._getTracker("#{tag.attr['id']}");
 pageTracker._trackPageview();
 } catch(err) {}</script>}
+    else
+%{<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("#{tag.attr['id']}");
+pageTracker._setDomainName(".#{tag.attr['domain']}");
+pageTracker._trackPageview();
+} catch(err) {}</script>}
+    end
   end
 end
--- spec/lib/google_analytics_spec.rb
+++ spec/lib/google_analytics_spec.rb
@@ -4,6 +4,7 @@ describe 'GoogleAnalytics' do
   dataset :pages
   
   describe '<r:google_analytics>' do
+
     it 'should render the correct HTML' do
       tag = '<r:google_analytics id="ID" />'
       
@@ -19,5 +20,23 @@ pageTracker._trackPageview();
 
       pages(:home).should render(tag).as(expected)
     end
+    
+    it 'should render the correct HTML' do
+      tag = '<r:google_analytics id="ID" type="m" domain="i.com" />'
+      
+      expected = %{<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("ID");
+pageTracker._setDomainName(".i.com");
+pageTracker._trackPageview();
+} catch(err) {}</script>}
+
+      pages(:home).should render(tag).as(expected)
+    end
+
   end
 end
